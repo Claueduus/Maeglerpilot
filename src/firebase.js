ca09d74d190fddef9ae5f7a2f19de207547c9d96
Claueduus <claudiasduus@gmail.com>
2025-05-19 15:18:06 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjOny1RCN1PJK9-2bT4-K0cAzAXUKaI4M",
-  authDomain: "maeglerpilot.firebaseapp.com",
-  projectId: "maeglerpilot",
-  storageBucket: "maeglerpilot.appspot.com",
+  apiKey: "AIzaSyAjOny1RCN1PJK9-2bT4-K0cAzAXUkaI4M",
+  authDomain: "maelgerpilot.firebaseapp.com",
+  projectId: "maelgerpilot",
+  storageBucket: "maelgerpilot.firebasestorage.app",
   messagingSenderId: "1039101910426",
   appId: "1:1039101910426:web:2b479f6f15cadce4a62e23"
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
 export const db = getFirestore(app);
